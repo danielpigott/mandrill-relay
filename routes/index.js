7c2ce023337752025fd48a58f33ee83086b1688d
Daniel Pigott <pigottd@gmail.com>
2015-05-21 21:59:39 +1000

    Initialised express app

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var _  = require('lodash');
+var config = require('config.json')('../sample.json');
+
+/*
+ * Send back a 500 error
+ */
+function handleError(res, error) {
+    return res.send(500, {error: error.message});
+}
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Mandrill Relay' });
+});
+
+router.post('/', function(req, res) {
+    var messages = JSON.parse(req.param('mandrill_events'));
+     console.log(messages);
+    _.each(messages, function(message) {
+        console.log(message);
+	    if (error) {
+            handleError(res, error) 
+	    }
+	});
+});
+
+module.exports = router;
