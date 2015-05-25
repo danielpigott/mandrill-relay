38ac15f8d06cdb4e051c094fbe2208575d62bc9e
Daniel Pigott <pigottd@gmail.com>
2015-05-25 21:28:32 +1000

    Got relay working

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
+var util = require('util');
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 var _  = require('lodash');
-var config = require('config.json')('../sample.json');
+var config = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
+console.log(config);
+var mandrill = require('node-mandrill')(config.key);
 
 /*
  * Send back a 500 error
@@ -16,11 +20,41 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
     var messages = JSON.parse(req.param('mandrill_events'));
-     console.log(messages);
     _.each(messages, function(message) {
-        console.log(message);
-	});
-  res.render('index', { title: 'Mandrill Relay' });
+      var sent = false;
+      var destinationParts = message.msg.email.split('@');
+      var destinationId = destinationParts[0];
+      var destinationDomain = destinationParts[1];
+      _.each(config.forwarders, function(forwarder) {
+        var forwardParts = forwarder.from.split('@');
+        var forwardId = forwardParts[0];
+        var forwardDomain = forwardParts[1] || '*';
+        if ((destinationDomain == forwardDomain || forwardDomain === '*') && (forwardId == destinationId || forwardId == '*')) {
+          sent = true;
+          var relayMessage = {
+            to: [{email: forwarder.to, name: message.msg.sender}],
+            from_email: util.format('relay@%s', destinationDomain),
+            from_name: util.format('%s via Relay', message.msg.from_name),
+            subject: util.format('Message to %s from %s: %s', message.msg.email,  message.msg.from_email, message.msg.subject),
+            headers: {
+              'Reply-To': message.msg.from_email
+            },
+            html: message.msg.html,
+            text: message.msg.text
+          };
+          mandrill('/messages/send', { message: relayMessage}, function(error, response) {
+              if (error) {
+                handleError(res, error);
+              } else {
+                res.json(response);
+              }
+          });
+        }
+      });
+      if (sent === false) {
+        handleError(res, {message: util.format('Forwarder not found for %s', message.msg.email)});
+      }
+  });
 });
 
 module.exports = router;
