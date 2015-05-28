40254a86c5866bd6743ff1ac9ea8ad8bdd8a1672
Daniel Pigott <pigottd@gmail.com>
2015-05-28 22:04:24 +1000

    Added signature checking

--- routes/index.js
+++ routes/index.js
@@ -6,53 +6,63 @@ var _  = require('lodash');
 var config = JSON.parse(fs.readFileSync(__dirname + '/../config.json'));
 console.log(config);
 var mandrill = require('node-mandrill')(config.key);
-
-/*
- * Send back a 500 error
- */
-function handleError(res, error) {
-    return res.send(500, {error: error.message});
-}
+var signing = require(__dirname + '/../lib/mandrill-signature');
 
 router.get('/', function(req, res) {
     res.render('index', { title: 'Mandrill Relay' });
 });
 
 router.post('/', function(req, res) {
-    var messages = JSON.parse(req.param('mandrill_events'));
-    _.each(messages, function(message) {
-      var sent = false;
-      var destinationParts = message.msg.email.split('@');
-      var destinationId = destinationParts[0];
-      var destinationDomain = destinationParts[1];
-      _.each(config.forwarders, function(forwarder) {
-        var forwardParts = forwarder.from.split('@');
-        var forwardId = forwardParts[0];
-        var forwardDomain = forwardParts[1] || '*';
-        if ((destinationDomain == forwardDomain || forwardDomain === '*') && (forwardId == destinationId || forwardId == '*')) {
-          sent = true;
-          var relayMessage = {
-            to: [{email: forwarder.to, name: message.msg.sender}],
-            from_email: util.format('relay@%s', destinationDomain),
-            from_name: util.format('%s via Relay', message.msg.from_name),
-            subject: util.format('Message to %s from %s: %s', message.msg.email,  message.msg.from_email, message.msg.subject),
-            headers: {
-              'Reply-To': message.msg.from_email
-            },
-            html: message.msg.html,
-            text: message.msg.text
-          };
-          mandrill('/messages/send', { message: relayMessage}, function(error, response) {
-              if (error) {
-                console.log(error.message);
-              } else {
-                console.log(JSON.stringify(response));
+    function sendMessages() {
+      var messages = JSON.parse(req.param('mandrill_events'));
+      _.each(messages, function(message) {
+        var sent = false;
+        var destinationParts = message.msg.email.split('@');
+        var destinationId = destinationParts[0];
+        var destinationDomain = destinationParts[1];
+        _.each(config.forwarders, function(forwarder) {
+          var forwardParts = forwarder.from.split('@');
+          var forwardId = forwardParts[0];
+          var forwardDomain = forwardParts[1] || '*';
+          if ((destinationDomain == forwardDomain || forwardDomain === '*') && (forwardId == destinationId || forwardId == '*')) {
+            if (config.auth === true) {
+              var calculatedSignature = signing.generateSignature(forwarder.authKey, config.url, req.body);
+              var requestSignature = req.get('X-Mandrill-Signature');
+              if (calculatedSignature !== requestSignature) {
+                console.log(util.format('Calculated Signature %s did not match sent signature %s', calculatedSignature, requestSignature));
+                throw {message: 'Error with request signature'};
               }
-          });
-        }
-      });
-  });
-  res.send('ok');
+            }
+            var relayMessage = {
+              to: [{email: forwarder.to, name: message.msg.sender}],
+              from_email: util.format('relay@%s', destinationDomain),
+              from_name: util.format('%s via Relay', message.msg.from_name),
+              subject: util.format('Message to %s from %s: %s', message.msg.email,  message.msg.from_email, message.msg.subject),
+              headers: {
+                'Reply-To': message.msg.from_email
+              },
+              html: message.msg.html,
+              text: message.msg.text
+            };
+            mandrill('/messages/send', { message: relayMessage}, function(error, response) {
+                if (error) {
+                  console.log(error.message);
+                } else {
+                  console.log(JSON.stringify(response));
+                }
+            });
+          }
+        });
+    });
+
+    return true;
+  };
+  try {
+    sendMessages();
+    res.send('ok');
+  } catch(e) {
+    res.status(500).send({error: e.message});
+  }
 });
 
 module.exports = router;
